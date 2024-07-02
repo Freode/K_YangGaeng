6ac178b27496c6c4f87f4e25232eb84a47ccd9d7
Freode <free4760@naver.com>
2024-07-02 09:57:44 +0900

    Main System Update

--- Source/K_YangGaeng/K_YangGaeng.h
+++ Source/K_YangGaeng/K_YangGaeng.h
@@ -1,5 +1,19 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// © 2024 Team 'K_YangGaeng'. All rights reserved.
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "EngineMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(K_YG, Log, All);
+
+// 호출 지점 출력
+#define K_YG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+
+// 간단한 로그 출력
+#define K_YG_SIMPLE(Verbosity) UE_LOG(K_YG, Verbosity, TEXT("%s"), *K_YG_CALLINFO)
+
+// 텍스트 형식을 추가한 로그 출력
+#define K_YG_UELOG(Verbosity, Format, ...) UE_LOG(K_YG, Verbosity, TEXT("%s %s"), *K_YG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+
+// Assertion : 확인
+#define K_YG_CHECK(Expr, ...) {if(!(Expr)) { K_YG_UELOG(Error, TEXT("Assertion : %s"), TEXT("'"#Expr"'")); return __VA_ARGS__; }}
