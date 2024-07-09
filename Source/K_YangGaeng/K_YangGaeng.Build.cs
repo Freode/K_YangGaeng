22b67c46ef4891febf397af357229e14e5307a85
FineDust1598 <135123667+FineDust1598@users.noreply.github.com>
2024-07-09 15:41:41 +0900

    test

--- Source/K_YangGaeng/K_YangGaeng.Build.cs
+++ Source/K_YangGaeng/K_YangGaeng.Build.cs
@@ -7,7 +7,6 @@ public class K_YangGaeng : ModuleRules
 	public K_YangGaeng(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "AnimGraphRuntime", "UMG", "Slate", "SlateCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "AnimGraphRuntime", "UMG", "Slate", "SlateCore" });
     }
 }
