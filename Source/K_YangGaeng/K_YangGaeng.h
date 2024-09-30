d9bef6ef6daa80c802ba1ef4957dc874b9feac3a
Freode <free4760@naver.com>
2024-09-30 20:31:05 +0900

    Update Character Head's Pitch and Mecro
    1. Update character head's pitch within range
    2. Add mecro in project's header file
    - Client number identifier function
    - Print customize text to viewport

--- Source/K_YangGaeng/K_YangGaeng.h
+++ Source/K_YangGaeng/K_YangGaeng.h
@@ -20,3 +20,58 @@ DECLARE_LOG_CATEGORY_EXTERN(K_YG, Log, All);
 
 // Simple Check : Not Assertion : 단순 조건문 확인 - return 코드 줄이는 매크로
 #define K_YG_SIMPLE_CHECK(Expr, ...) {if(!(Expr)) { K_YG_UELOG(Warning, TEXT("The function is pre-termination : %s"), TEXT("'"#Expr"'")); return __VA_ARGS__; }}
+
+// Print text on viewport
+#define K_YG_PRINT(Time, Color, Format, ...) {if(GEngine){GEngine->AddOnScreenDebugMessage(-1, Time, Color, FString::Printf(Format, ##__VA_ARGS__));}}
+
+// Print text on viewport during a frame
+#define K_YG_PRINT_A_FRAME(Color, Format, ...){if(GEngine){GEngine->AddOnScreenDebugMessage(-1, FApp::GetDeltaTime(), Color, FString::Printf(Format, ##__VA_ARGS__));}}
+
+// Cached client name
+static FString CachedClientIdentifier = TEXT("UnknownClient");
+
+//
+#define K_YG_CLIENT_IDENTIFIER()(GetClientIdentifier())
+
+//
+inline FString GetClientIdentifier()
+{
+	// When cached client name is valid, return it
+	if (CachedClientIdentifier.Equals(TEXT("UnknownClient")) == false)
+	{
+		return CachedClientIdentifier;
+	}
+
+    // GEngine is valid
+	if (!GEngine) { return FString("UnknownClient"); }
+
+    // Check current client number
+    for (const FWorldContext& WorldContext : GEngine->GetWorldContexts())
+    {
+        // Find game world
+        if (WorldContext.World() && WorldContext.World()->IsGameWorld())
+        {
+            UWorld* World = WorldContext.World();
+
+            // If current controller is server, return server text
+            if (World->IsServer())
+            {
+                CachedClientIdentifier = FString("Server");
+                return CachedClientIdentifier;
+            }
+            // When it isn't server, return client & number which is decided by game mode
+            else
+            {
+                APlayerController* PC = World->GetFirstPlayerController();
+                if (PC && PC->GetLocalPlayer())
+                {
+                    CachedClientIdentifier = FString::Printf(TEXT("Client_%d"), PC->GetLocalPlayer()->GetControllerId());
+                    return CachedClientIdentifier;
+                }
+            }
+        }
+    }
+       
+    // When it isn't find, return previous name
+	return FString("UnknownClient");
+}
